bc9bb291dbdfbd266d9bb4a3c99690e886f9103e
Robert <robertlopezcaballero13@gmail.com>
2025-05-25 23:37:07 -0400

    version 1.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import { Temporizador } from './componets/temporizador'
 import './styles/mainStyles.css'
 import './App.css'
 
-
-
 const cuadros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8]
 
 function App() {
